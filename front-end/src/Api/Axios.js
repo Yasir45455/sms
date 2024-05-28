b37acb16a2331d750554257befa6f2d061818f40
Yasir45455 <149455805+Yasir45455@users.noreply.github.com>
2024-05-28 11:37:13 +0500

    Update Axios.js

--- front-end/src/Api/Axios.js
+++ front-end/src/Api/Axios.js
@@ -3,5 +3,5 @@ export default axios.create({
     // baseURL:'http://192.168.100.57:8000/api'
     // baseURL:'http://192.168.100.2:8000/api'
     // baseURL:'http://192.168.100.10:8080/api'
-    baseURL:'http://localhost:4000/api'
+   baseURL:'http://178.128.207.34/api'
 })
